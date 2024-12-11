f546a435185503aaad4ff81dc5a74d95da86eedb
gabriel-Oak <gabrielcarvalhocosta@live.com>
2024-12-11 10:48:14 -0300

    --

---
+++ conversoes/conversoes.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	x := 2.4
+	y := 2
+	fmt.Println(x / float64(y))
+
+	nota := 6.9
+	notaFinal := int(nota)
+	fmt.Println(notaFinal)
+
+	// Cuidado...
+	fmt.Println("Teste " + string(97)) // 97 é o valor na tabela Unicode
+
+	// int para string
+	fmt.Println("Teste " + strconv.Itoa(123))
+
+	// string para int
+	num, _ := strconv.Atoi("123")
+	fmt.Println(num - 122)
+
+	// string para boolean
+	b, _ := strconv.ParseBool("true")
+	if b {
+		fmt.Println("Verdadeiro")
+	}
+}
