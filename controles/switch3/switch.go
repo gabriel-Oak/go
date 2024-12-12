9b49a736fa695d58de358896d4f4d30f496133a5
gabriel-Oak <gabrielcarvalhocosta@live.com>
2024-12-12 12:15:40 -0300

    --

---
+++ controles/switch3/switch.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func tipo(i interface{}) string {
+	switch i.(type) {
+	case int:
+		return "inteiro"
+	case float32, float64:
+		return "real"
+	case string:
+		return "texto"
+	case func():
+		return "função"
+	default:
+		return "desconhecido"
+	}
+}
+
+func main() {
+	fmt.Println(tipo(2.3))
+	fmt.Println(tipo(1))
+	fmt.Println(tipo("Opa"))
+	fmt.Println(tipo(func() {}))
+	fmt.Println(tipo(true))
+}
